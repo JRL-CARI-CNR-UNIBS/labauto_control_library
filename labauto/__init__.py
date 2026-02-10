63b0dc6d2aab3ffa450df3380c4a2691c42ccddc
Manuel Beschi <manuel.beschi@unibs.it>
2026-02-10 13:58:58 +0100

    add mujoco wrapper

--- labauto/__init__.py
+++ labauto/__init__.py
@@ -12,6 +12,7 @@ from .load_controller import loadController
 
 from .mechanical_system import MechanicalSystem
 from .pinocchio_robotic_system import PinocchioRoboticSystem
+from .mujoco_robotic_system import PinocchioRoboticSystem
 
 from .motion_law import MotionLaw
 from .trapezoidal_motion_law import TrapezoidalMotionLaw
